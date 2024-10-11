dcb8879a7b1d379e44de15c7bf7ea0b546268fa6
veeranji <141345449+Veeeranji11@users.noreply.github.com>
2024-10-11 12:27:06 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,10 @@
+public class Swap{
+    public static void main(String[] args){
+        int temp=0,a=10,b=12;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
