4e56b4ace09198caee338b4e3a4f0c2e205b7d06
veeranji <141345449+Veeeranji11@users.noreply.github.com>
2024-10-11 12:26:38 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,13 @@
+public class Even{
+    public static void main(String[] args){
+        int a=9;
+        if(a%2==0)
+        {
+            System.out.println("even number");
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+}
