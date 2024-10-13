12c161b486ebd887831cdb81413432bca748b0f0
veeranji <141345449+Veeeranji11@users.noreply.github.com>
2024-10-13 19:13:51 +0530

    Add files via upload

---
+++ Vote.java
@@ -0,0 +1,13 @@
+public class Vote{
+    public static void main(String[] args){
+        int y=19;
+        if(y>=19)
+        {
+        System.out.println("eligible");
+        }
+        else
+        {
+            System.out.println("not eligible");
+        }
+    }
+}
